6c53d54e86adcd91572ab97a8170bdbf17e8a947
Sharthak1705 <sharthakjain1@gmail.com>
2025-06-05 18:20:32 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+const app = express();
+import cookieParser from 'cookie-parser';
+import messageRoutes from './routes/message.route.js';
+
+import dotenv from "dotenv";
+import { connectDB } from './lib/db.js';
+import authRoute from './routes/auth.route.js';
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth",authRoute);
+app.use("api/message", messageRoutes);
+
+app.listen(PORT,() =>{
+    console.log(`Server is running on port:${PORT}`);
+   connectDB();
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () =>{
+  return (
+    <div className="h-10 w-10">
+       <h1> Welcome to the chat Application</h1>
+    </div>
+  )
+}
+
+export default App;
